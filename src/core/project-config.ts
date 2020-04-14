b73069e5661299b19c1b05c9563874216504aeed
yarastqt <yarastqt@gmail.com>
2020-04-15 00:20:29 +0300

    chore: expose config type

--- src/core/project-config.ts
+++ src/core/project-config.ts
@@ -1,8 +1,18 @@
 import { join } from 'path'
 import { readJsonSync } from 'fs-extra'
 
-type Config = {
-  platforms: {}
+import { Platforms } from './platforms'
+
+export type Config = {
+  rootDir: string
+  exclude: string[]
+  platforms: Platforms
+  formats: {
+    [key: string]: {
+      transforms: string[]
+      options?: {}
+    }
+  }
 }
 
 export async function getProjectConfig(source: string, configName: string): Promise<Config> {
