1049ff5966d8082df7ca4d4a3f746d76a3f074fe
yarastqt <yarastqt@gmail.com>
2020-04-14 16:40:34 +0300

    feat: merge nested themes for client api

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import toColor from 'color'
+import Color from 'color'
+import deepmerge from 'deepmerge'
 
 import { ThemeTokens } from './core/token.h'
 
-export function theme(shape1: ThemeTokens, shape2?: ThemeTokens): ThemeTokens {
-  return shape1
+export function theme(shape1: ThemeTokens, shape2: ThemeTokens = {}): ThemeTokens {
+  return deepmerge(shape1, shape2)
 }
 
 export function color(inputColor: string, options: { h?: number; s?: number; l?: number }): string {
-  let color = toColor(inputColor)
-  return color.hsl(options.h, options.s, options.l).hex()
+  return Color(inputColor)
+    .hsl(options.h, options.s, options.l)
+    .hex()
 }
