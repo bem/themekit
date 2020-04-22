4d26ffd1a193cadd5c5184e907938ad649e84ad2
yarastqt <yarastqt@gmail.com>
2020-04-22 19:36:03 +0300

    featt: support ts tokens

--- src/core/import-module.ts
+++ src/core/import-module.ts
@@ -1,3 +1,15 @@
+import { transpileModule } from 'typescript'
+import { readFile } from 'fs-extra'
+// @ts-ignore
+import neval from 'node-eval'
+
+export function esModuleInterop<T>(box?: T): T {
+  if (box === undefined) {
+    return box as any
+  }
+  return (box as any).default || box
+}
+
 /**
  * Import module with esm interop.
  *
@@ -5,6 +17,9 @@
  * @return Promise with data.
  */
 export async function importModule<T>(path: string): Promise<T> {
-  const maybeModule = await import(path)
-  return maybeModule.default || maybeModule
+  const source = await readFile(path, 'utf-8')
+  // TODO: Add diagnostic.
+  const transpileResult = transpileModule(source, {})
+  const result = neval(transpileResult.outputText, path)
+  return esModuleInterop(result)
 }
