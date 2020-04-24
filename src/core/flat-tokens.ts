7b65228b8c01bb3a24a9b4bfa94be2252479ac7e
yarastqt <yarastqt@gmail.com>
2020-04-24 16:06:11 +0300

    refactor: add type-guard for TokenMap

--- src/core/flat-tokens.ts
+++ src/core/flat-tokens.ts
@@ -1,6 +1,35 @@
 import { withPrefix } from './with-prefix'
 import { Shape, FlattenToken, Token, TokensMap } from './token.h'
 
+/**
+ * Returns flatten shape with tokens.
+ *
+ * @param tokens Map with tokens.
+ */
+export function flatTokens(tokens: TokensMap, prefix?: string): Shape<FlattenToken> {
+  const result: Shape<FlattenToken> = {}
+  for (const key in tokens) {
+    const computedKey = withPrefix(key, prefix)
+    const token = tokens[key]
+    if (typeof token === 'string') {
+      result[computedKey] = {
+        value: token,
+        type: getTokenType(token),
+        name: computedKey,
+      }
+    }
+    else if (isTokenMap(token)) {
+      Object.assign(result, flatTokens(token, computedKey))
+    } else {
+      result[computedKey] = {
+        ...token,
+        name: computedKey,
+      }
+    }
+  }
+  return result
+}
+
 function getTokenType(value: string): Token['type'] {
   // TODO: Order is necessary, cuz color maybe has percent.
   if (value.match(/#|rgb|hsl/) !== null) {
@@ -12,31 +41,6 @@ function getTokenType(value: string): Token['type'] {
   return 'unknown'
 }
 
-/**
- * Returns flatten shape with tokens.
- *
- * @param tokens Map with tokens.
- */
-export function flatTokens(tokens: TokensMap, __prefix__?: string): Shape<FlattenToken> {
-  const result: Shape<FlattenToken> = {}
-  for (const key in tokens) {
-    const transformedKey = withPrefix(key, __prefix__)
-    const maybeToken = tokens[key]
-    if (typeof maybeToken === 'string') {
-      result[transformedKey] = {
-        value: maybeToken,
-        type: getTokenType(maybeToken),
-        name: transformedKey,
-      }
-    }
-    else if (maybeToken.value === undefined) {
-      Object.assign(result, flatTokens(maybeToken as TokensMap, transformedKey))
-    } else {
-      result[transformedKey] = {
-        ...maybeToken as Token,
-        name: transformedKey,
-      }
-    }
-  }
-  return result
+function isTokenMap(token: Token | TokensMap): token is TokensMap {
+  return token.value === undefined
 }
