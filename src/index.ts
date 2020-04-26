d8abc068f3077321b4ed9791b641ee282c4d748a
yarastqt <yarastqt@gmail.com>
2020-04-26 03:13:26 +0300

    feat: add generic type for primitives

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
 import deepmerge from 'deepmerge'
 
-import { Shape, Primitive, ThemeTokens } from './core/types'
+import { Shape, ThemeTokens } from './core/types'
 import { esModuleInterop } from './core/import-module'
 
-type Primitives = Shape<Primitive>
-type ThemeFn = ThemeFn1 | ThemeFn2
-type ThemeFn1 = (primitives: Primitives) => ThemeTokens
-type ThemeFn2 = (primitives: Primitives) => ThemeFn1
+type ThemeFn<T> = ThemeFn1<T> | ThemeFn2<T>
+type ThemeFn1<T> = (primitives: T) => ThemeTokens
+type ThemeFn2<T> = (primitives: T) => ThemeFn1<T>
 
 // TODO: theme args must be fn or plain shape.
-export function withTokens(themeFn1: ThemeFn, themeFn2?: ThemeFn) {
-  return (primitives1: Primitives = {}) => (primitives2: Primitives = {}) => {
-    const composedPrimitives = deepmerge(primitives1, primitives2)
-    const theme1 = esModuleInterop<ThemeFn1>(themeFn1 as any)
-    const theme2 = esModuleInterop<ThemeFn1>(themeFn2 as any)
+export function withTokens<T extends Shape<any>>(themeFn1: ThemeFn<T>, themeFn2?: ThemeFn<T>) {
+  return (primitives1?: T) => (primitives2?: T) => {
+    const composedPrimitives = deepmerge<T>(primitives1 || {}, primitives2 || {})
+    const theme1 = esModuleInterop<ThemeFn1<T>>(themeFn1 as any)
+    const theme2 = esModuleInterop<ThemeFn1<T>>(themeFn2 as any)
     if (theme2 === undefined) {
       return theme1(composedPrimitives)
     }
