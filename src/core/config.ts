3eedd0e6e84640f2fb1beaaf32b1ead91a929e6a
Eugene <yarastqt@gmail.com>
2020-05-09 16:40:51 +0300

    wip

--- src/core/config.ts
+++ src/core/config.ts
@@ -1,12 +1,31 @@
 import { Platforms } from './platforms'
 
+// TODO: update readme
 export type Config = {
+  /**
+   * Tokens sources
+   */
   src: string
+  /**
+   * Platforms
+   */
   platforms?: Platforms
+  /**
+   * Formats
+   */
   formats: {
     [key: string]: {
+      /**
+       * Result build
+       */
       outDir: string
+      /**
+       * Result fiflename
+       */
       fileName?: string
+      /**
+       * Applied transformation for tokens
+       */
       transforms: string[]
     }
   }
