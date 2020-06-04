b6f7d15c65679521da33dad70af4d2b20872288a
Eugene <yarastqt@gmail.com>
2020-06-04 03:02:32 +0300

    fix: suppress extended error while yml parse

--- src/core-v2/yaml-interop.ts
+++ src/core-v2/yaml-interop.ts
@@ -6,7 +6,9 @@ Module.prototype.require = new Proxy(Module.prototype.require, {
   apply(target, thisArg, args) {
     if (/\.ya?ml$/.test(args[0])) {
       const file = readFileSync(args[0], 'utf8')
-      return YAML.parse(file)
+      try {
+        return YAML.parse(file)
+      } catch (error) {}
     }
     return Reflect.apply(target, thisArg, args)
   },
