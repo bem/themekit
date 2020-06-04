b3cd2aa41af9ee98580a00f16b8a5d0a62b8cea0
Eugene <yarastqt@gmail.com>
2020-06-04 17:35:30 +0300

    feat: support sources as single string

--- src/core-v2/themes.ts
+++ src/core-v2/themes.ts
@@ -13,9 +13,10 @@ function resolveRootDir(filePath: string): string {
     : filePath
 }
 
-export async function loadThemes(sources: string[]): Promise<Themes> {
+export async function loadThemes(sources: string | string[]): Promise<Themes> {
   const result: Themes = { mappers: [], sources: [], whitepaper: {} }
-  for (const sourcePath of sources) {
+  const normalizedSources = Array.isArray(sources) ? sources : [sources]
+  for (const sourcePath of normalizedSources) {
     const { mappers = [], sources = [], whitepaper }: Themes = await readJSON(sourcePath)
     result.mappers.push(...mappers.map(resolveRootDir))
     result.sources.push(...sources.map(resolveRootDir))
