f5ba22e5c0bcbe8aa28ec1abf2736ec2ebc84aa6
Eugene <yarastqt@gmail.com>
2020-06-08 02:30:24 +0300

    chore: remove legacy core

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -1,7 +1,3 @@
-declare module 'node-eval' {
-  export default function(content: string, filename: string, context?: {}): any
-}
-
 declare module 'css-color-function' {
   namespace cssColorFunction {
     function convert(value: string): string
