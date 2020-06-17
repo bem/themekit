f28d740c3ecaaa818ff117426ea4c5a72fedfb7c
Eugene <yarastqt@gmail.com>
2020-06-17 16:45:02 +0300

    fix: fix sources order with extends

--- src/core/load-sources.ts
+++ src/core/load-sources.ts
@@ -3,22 +3,20 @@ import glob from 'fast-glob'
 import { Platforms, platforms } from '../core/platforms'
 import { getPlatformFromFilePath } from './utils'
 
-export async function loadSources(path: string[], platform: Platforms): Promise<string[]> {
+export async function loadSources(paths: string[][], platform: Platforms): Promise<string[]> {
   const levels = platforms.get(platform)
 
   if (levels === undefined) {
     throw new Error(`Unexpected platform: ${platform}, please check configuration.`)
   }
 
-  const files = await glob(path)
-
-  const result = files
+  // Uses nested array with paths, cuz glob not save orders with using patterns for path.
+  const result = (await Promise.all(paths.map((path) => glob(path))))
+    .flat()
     .filter((file) => {
       const filePlatform = getPlatformFromFilePath(file)
       return levels.includes(filePlatform as Platforms)
     })
-    // Use dual sort for idempotent sorting.
-    .sort()
     .sort((a, b) => {
       const a1 = getPlatformFromFilePath(a)
       const b1 = getPlatformFromFilePath(b)
