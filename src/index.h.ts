3c772bb1a6dd96d3c90a4fc843f0448daa637b5c
Eugene <yarastqt@gmail.com>
2020-11-28 06:21:48 +0300

    chore: add type for Prop

--- src/index.h.ts
+++ src/index.h.ts
@@ -1,3 +1,30 @@
+export type Prop = {
+  /**
+   * Property additional attributes
+   */
+  attributes: Record<string, string>
+  /**
+   * Property comment
+   */
+  comment?: string
+  /**
+   * Property name
+   */
+  name: string
+  /**
+   * Property original value with comment
+   */
+  original: { value: string; comment?: string }
+  /**
+   * Property nested path
+   */
+  path: string[]
+  /**
+   * Property value
+   */
+  value: string
+}
+
 export type Preset = {
   /**
    * Preset name
