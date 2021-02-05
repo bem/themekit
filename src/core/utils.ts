dd0229ea15ee3bb7857cf12aaf321f96294c628c
cruperman <temashu@ya.ru>
2021-02-05 16:14:43 +0300

    fix: fix broken sources

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,9 +1,11 @@
+import { basename } from 'path'
 import normalize from 'normalize-path'
 
 import { Platforms } from '../core/platforms'
 
 export function getPlatformFromFilePath(filePath: string): Platforms {
-  const matched = filePath.match(/@([\w|-]+)+\./)
+  const fileName = basename(filePath)
+  const matched = fileName.match(/@([\w|-]+)+\./)
   return matched === null ? 'common' : (matched[1] as Platforms)
 }
 
